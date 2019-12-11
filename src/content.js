bd9c6de25b41187ec6a34f16e76922332587f7d3
suresh-44 <sureshsunsuri@gmail.com>
2019-12-11 18:33:20 +0530

    changed manifest.json

--- src/content.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action") {
-      var firstHref ="this is test";
-
-      console.log(firstHref);
-    }
-  }
-);
